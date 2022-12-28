106a8f3f2a7d8f33d674330d8c81f624494f51c1
temakuch <ku4eruck2005@gmail.com>
2022-12-29 00:42:08 +0200

    few comments and resizing

--- upscale.py
+++ upscale.py
@@ -15,3 +15,15 @@ def upscale_nn(im):
  
     result = sr.upsample(img)
     return result
+
+img = cv2.imread("gelikopter.jpg")
+sr = cv2.dnn_superres.DnnSuperResImpl_create()
+path = "LapSRN_x8.pb"
+ 
+sr.readModel(path)
+ 
+sr.setModel("lapsrn",8)
+ 
+result = sr.upsample(img)
+cv2.imshow("result", result)
+cv2.waitKey(10000000)
--- zone.py
+++ zone.py
@@ -1,6 +1,4 @@
 import cv2 as cv
-import time
-from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 import multiprocessing
@@ -20,7 +18,7 @@ res = 0
 old_res = 0
 k = 0
 
-
+# Функція знаходження координат виділеної зони
 def coords(event,mouseX,mouseY, flags, param):
     global startX, startY, endX, endY 
     if event == cv.EVENT_LBUTTONDOWN:
@@ -29,6 +27,7 @@ def coords(event,mouseX,mouseY, flags, param):
         endX, endY = mouseX, mouseY
     return (startX, startY, endX, endY)
 
+# Функція малювання прямокутника по заданим координатам
 def draw_rectangle():
     global rect
     if (endX>0 and endY>0):
@@ -37,7 +36,7 @@ def draw_rectangle():
     else: rect = False    
     
 
-def cutting():
+"""def cutting():
     global rect
     if rect == True:   
         cut_img = frame[startY+1:endY-1, startX+1:endX-1]
@@ -45,26 +44,34 @@ def cutting():
         plt.imshow(result[:,:,::-1])    
         plt.show()
     else:
-        rect = False
-
+        rect = False """
+# Фукнція для роботи асинхронної обробки зображення 
 def callbacking(temp_res):
-    global cut_img, rect,res
+    global cut_img, rect, res
     if rect == True:
         cut_img = frame[startY+1:endY-1, startX+1:endX-1]
     pool.apply_async(upscale_nn, args=(cut_img,), callback=callbacking)
     if isinstance(temp_res, np.ndarray):
         res = temp_res
+
+# Функція виводу результату після апскейлу
 def show_result():
-    global res, old_res
+    global res, old_res, k, event 
     while 1:
         if isinstance(res, np.ndarray):
             if not np.array_equal(res, old_res):
-                cv.imshow('Upscaled',res[:,:,::-1])
-                cv.waitKey(1)
+                im = res[:,:,::-1]
+                width = int(im.shape[1]*0.4)
+                height = int(im.shape[0]*0.4)
+                size = (width, height)
+                resized_im = cv.resize(im, size)
+                cv.imshow('Upscaled',resized_im)
+                k = cv.waitKey(1)
                 old_res = res
                 if k == 27:
                     cv.destroyWindow('Upscaled')
                     break
+
 ret, first_frame = video.read()
 cut_img = first_frame[0:40, 0:40]
 if __name__=="__main__":
@@ -73,7 +80,6 @@ if __name__=="__main__":
     t = td.Thread(target = show_result)
     t.start()
     while True:
-        #print("CV@CV@CV@CV@CV@CV@CV@CV@CV@v")
         ret, frame = video.read()
         cv.namedWindow('Frame')
         cv.setMouseCallback('Frame',coords)
@@ -83,9 +89,7 @@ if __name__=="__main__":
         if k == 27:
             break
         if k == ord("p"):
-            #cutting()
             cv.waitKey(-1)
-
 
 
             
