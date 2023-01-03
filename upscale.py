cb712e5f25a2b350833dafb3c9bef468635d36f7
temakuch <ku4eruck2005@gmail.com>
2023-01-03 12:07:22 +0200

    changed scale x8 - x4

--- upscale.py
+++ upscale.py
@@ -7,23 +7,25 @@ def upscale_nn(im):
     img = im # cv2.imread(im)
     sr = cv2.dnn_superres.DnnSuperResImpl_create()
  
-    path = "LapSRN_x8.pb"
+    path = "LapSRN_x4.pb"
  
     sr.readModel(path)
  
-    sr.setModel("lapsrn",8)
+    sr.setModel("lapsrn",4)
  
     result = sr.upsample(img)
     return result
 
+"""timer = time.time()
 img = cv2.imread("gelikopter.jpg")
 sr = cv2.dnn_superres.DnnSuperResImpl_create()
-path = "LapSRN_x8.pb"
+path = "LapSRN_x4.pb"
  
 sr.readModel(path)
  
-sr.setModel("lapsrn",8)
+sr.setModel("lapsrn",4)
  
 result = sr.upsample(img)
 cv2.imshow("result", result)
-cv2.waitKey(10000000)
+print(time.time() - timer)
+cv2.waitKey(10000000)"""
